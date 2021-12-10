b9108f9178168ca40406c4cf265f432b28adefd7
Tamar Yogev <tamaryogev@Tamars-Air.Home>
2021-12-10 10:23:40 +0200

    before starting to work on region data object

--- scripts/main.js
+++ scripts/main.js
@@ -6,43 +6,95 @@ const urlObject = {
     covidAPI: "https://corona-api.com/countries"
 }
 
-const countriesArr = [];
+const countriesObject = {};
+let covidDataArr = [];
+
+COVID_DATA_OBJECT = { confirmed: 0, deaths: 0, critical: 0, recovered: 0 }
+
+
+const getCountriesData = async (obj) => {
+    const { data } = await axios.get(obj.proxy + obj.countryAPI);
+    data.forEach(
+        (country) => {
+            countriesObject[country.cca2] = {
+                name: country.name.common,
+                region: country.region,
+                code: country.cca2
+            }
+        }
+    )
+    // data.forEach(
+    //     (country) => {
+    //         covidDataArr.push(
+    //             {
+    //                 name: country.name.common,
+    //                 region: country.region,
+    //                 code: country.cca2
+    //             })
+    //     }
+
+    // )
+
+}
+
+const getCovidData = async (obj) => {
+    const { data: { data } } = await axios.get(obj.proxy + obj.covidAPI);
+
+    data.forEach((country) => {
+        countriesObject[country.code].covidData = country.latest_data;
+
+    })
+}
+
+
+const objectToArray = () => {
+    const result = Object.keys(countriesObject).map(function (key) {
+        return countriesObject[key];
+    })
+    return result;
+}
+
 
 // On the first click in the page gets the data from the api
 const getAllData = async (obj) => {
-    // const allCovidData = await axios.get(obj.proxy + obj.covidAPI);
+    await getCountriesData(obj)
+    await getCovidData(obj)
+    covidDataArr = await objectToArray();
+    await getRegionData("Europe")
+    // getRegions();
+}
 
-    // getCountriesData = 
-    // const allCountriesData = await axios.get(obj.proxy + obj.countryAPI);
-    const {data} = await axios.get(obj.proxy + obj.countryAPI);
-    console.log(data);
-    // console.log(allCovidData)
-    data.forEach(
-        (country) => { countriesArr.push
-             ( {
-                name: country.name.common,
-                code: country.cca2,
-                region: country.region
-            }
-             )
 
-        })
+// TODO: creat an array of regions
+// const getRegions = () => {
+//     const result = covidDataArr.reduce((previousValue, currentValue) => {
+//         !(previousValue.includes(currentValue))? 
+//             previousValue.push(currentValue.region):
+//             previousValue
+
+//     }, []);
+//     console.log(result)
+// }
+
+
+
+const getRegionData = async (selectedRegion) => {
+    const regionData = covidDataArr.filter((country) => ((country.region === selectedRegion)));
+    const countriesInRegion = regionData.map(country => country.name);
+    const countriesCovidData = regionData.map(country => {
+        return country.covidData ? country.covidData : COVID_DATA_OBJECT
+    });
+    // TODO: change to DRY
+    const countriesDeaths = countriesCovidData.map(x => x.deaths);
+    const countriesConfirmed = countriesCovidData.map(x => x.confirmed)
+    const countriesCritical = countriesCovidData.map(x => x.critical)
+    const countriesRecovered = countriesCovidData.map(x => x.recovered)
+
+    console.log(covidDataArr)
 }
 
 
 getAllData(urlObject);
-console.log (countriesArr)
-
-countriesObject = {
-    Asia: [
-        {
-            name: "",
-            code: ""
-        }],
-
-
-}
-
 
 
 
